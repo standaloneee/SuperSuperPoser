60b746471e50464fc97645c3ac79cd4a3f79362f
standaloneee <tzherzdev@gmail.com>
2022-04-14 19:22:42 +0300

    Добавил сообщение о скачивании бд при запуске. Переопределил логику закрытия приложения

--- PoserParser/MainWindow.xaml.cs
+++ PoserParser/MainWindow.xaml.cs
@@ -17,16 +17,17 @@ using System.IO;
 using PoserParser;
 using DocumentFormat.OpenXml.Wordprocessing;
 using Microsoft.Win32;
+using System.ComponentModel;
 
 namespace PoserParser
 {
 
-	/// <summary>
-	/// Логика взаимодействия для MainWindow.xaml
-	/// </summary>
-	// https://bdu.fstec.ru/files/documents/thrlist.xlsx
-	/// 
-	public partial class MainWindow : Window
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    // https://bdu.fstec.ru/files/documents/thrlist.xlsx
+    /// 
+    public partial class MainWindow : Window
     {
         const string address = "https://bdu.fstec.ru/files/documents/thrlist.xlsx";
         public List<UBIFull> History = new List<UBIFull>();
@@ -38,44 +39,51 @@ namespace PoserParser
         //допилить Report
         public MainWindow()
         {
-            
+
             try
             {
                 WsParse(new XLWorkbook(@"thrlist.xlsx").Worksheet("Sheet"), ref dataBase.shortDB, ref dataBase.fullDB);
             }
             catch (Exception)
             {
+                MessageBox.Show($"Базы данных на данном устройстве не обнаружено! База данных будет загружена с сервера!", "Уведомление");
                 new WebClient().DownloadFile(address, "thrlist.xlsx");
                 WsParse(new XLWorkbook(@"thrlist.xlsx").Worksheet("Sheet"), ref dataBase.shortDB, ref dataBase.fullDB);
+
             }
 
             this.cview = new PagingCollectionView(dataBase.shortDB, elmCount);
             this.DataContext = this.cview;
             InitializeComponent();
+
             if (History.Count == 0)
             {
                 HistoryButton.Visibility = Visibility.Hidden;
             }
+            
         }
-
+        protected override void OnClosing(CancelEventArgs e)
+        {            
+            Application.Current.Shutdown();
+        }
         private void WsParse(IXLWorksheet ws, ref Dictionary<int, UBIShort> dicShort, ref Dictionary<int, UBIFull> dic)
-		{
-			dicShort = new Dictionary<int, UBIShort>();
-			dic = new Dictionary<int, UBIFull>();
-			for (int i = 3; i <= ws.Rows().Count(); i++)
-			{
-				dicShort.Add(int.Parse(ws.Cell(i, 1).GetString()), 
+        {
+            dicShort = new Dictionary<int, UBIShort>();
+            dic = new Dictionary<int, UBIFull>();
+            for (int i = 3; i <= ws.Rows().Count(); i++)
+            {
+                dicShort.Add(int.Parse(ws.Cell(i, 1).GetString()),
                     new UBIShort("УБИ." + int.Parse(ws.Cell(i, 1).GetString()).ToString("000"), ws.Cell(i, 2).GetString()));
 
-				dic.Add(int.Parse(ws.Cell(i, 1).GetString()), new UBIFull(
-                    ws.Cell(i, 1).GetString(),ws.Cell(i, 2).GetString(),
-                    ws.Cell(i, 3).GetString(),ws.Cell(i, 4).GetString(), 
+                dic.Add(int.Parse(ws.Cell(i, 1).GetString()), new UBIFull(
+                    ws.Cell(i, 1).GetString(), ws.Cell(i, 2).GetString(),
+                    ws.Cell(i, 3).GetString(), ws.Cell(i, 4).GetString(),
                     ws.Cell(i, 5).GetString(),
                     ws.Cell(i, 6).GetString() == "0" ? "нет" : "да",
                     ws.Cell(i, 7).GetString() == "0" ? "нет" : "да",
                     ws.Cell(i, 7).GetString() == "0" ? "нет" : "да"));
-			}
-		}
+            }
+        }
 
         private void OnNextClicked(object sender, RoutedEventArgs e)
         {
@@ -87,8 +95,9 @@ namespace PoserParser
             this.cview.MoveToPreviousPage();
         }
 
-		private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
+        async private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            await Task.Delay(3000);
             ComboBoxItem comboBoxItem = ((sender as ComboBox).SelectedItem as ComboBoxItem);
             elmCount = int.Parse(comboBoxItem.Content.ToString());
             cview.ItemsPerPage = elmCount;
@@ -128,7 +137,7 @@ namespace PoserParser
                             if (i2.Value.AccessibiltiyCheck != i1.Value.AccessibiltiyCheck)
                                 props += i2.Value.AccessibiltiyCheck + "/";
                             //props = props.Remove(props.Length - 1);
-                             
+
                             changes.Add(i1.Key, props);
                             changed.Add(i2.Value);
                             changed.Add(i1.Value);
@@ -191,29 +200,29 @@ namespace PoserParser
         }
 
         private void UBI_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-		{
+        {
             var id = (UBI.SelectedItem as UBIShort).ID.ToString();
             var fullinfo = new FullInfo(id);
             var el = dataBase.fullDB[int.Parse(id.Split('.')[1])];
-			var smth = new List<object>
-			{
-				new
-				{
-					ID = el.ID,
-					Name = el.Name,
-					Description = el.Description,
-					HazardSource = el.HazardSource,
-					HazardObject = el.HazardObject,
-					ConfidentialCheck = el.ConfidentialCheck,
-					IntegrityCheck = el.IntegrityCheck,
-					AccessibiltiyCheck = el.AccessibiltiyCheck
-				}
-			};
-			fullinfo.DataContext = smth;
+            var smth = new List<object>
+            {
+                new
+                {
+                    ID = el.ID,
+                    Name = el.Name,
+                    Description = el.Description,
+                    HazardSource = el.HazardSource,
+                    HazardObject = el.HazardObject,
+                    ConfidentialCheck = el.ConfidentialCheck,
+                    IntegrityCheck = el.IntegrityCheck,
+                    AccessibiltiyCheck = el.AccessibiltiyCheck
+                }
+            };
+            fullinfo.DataContext = smth;
             fullinfo.Show();
-		}
+        }
 
-		private void Button_Save(object sender, RoutedEventArgs e)
+        private void Button_Save(object sender, RoutedEventArgs e)
         {
             Stream myStream;
             SaveFileDialog save = new SaveFileDialog();
@@ -221,14 +230,14 @@ namespace PoserParser
             save.Filter = "XLSX File (*.xlsx)|*.xlsx|All files (*.*)|*.*";
             save.RestoreDirectory = true;
 
-            try 
-            { 
+            try
+            {
                 save.ShowDialog();
-				if ((myStream = save.OpenFile()) != null)
-					myStream.Close();
-			}
-			catch (Exception) { }
-		}
+                if ((myStream = save.OpenFile()) != null)
+                    myStream.Close();
+            }
+            catch (Exception) { }
+        }
 
         private void Change_History(object sender, RoutedEventArgs e)
         {
